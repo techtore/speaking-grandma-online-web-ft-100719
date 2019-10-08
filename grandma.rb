f49fa42a1be2c85da5a47f556e288c84ea77fa12
Victoria Hale <vic.hale74@gmail.com>
2019-10-08 21:25:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase = phrase.upcase
+  puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
   # Write a speak_to_grandma method. *
